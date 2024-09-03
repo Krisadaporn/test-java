c4675e04f5fd240bced3e2c31c59ac64acf39c3b
Krisadaporn Petchdee <krisadaporn.p@gmail.com>
2024-09-03 16:52:12 +0700

    update

--- insecure.js
+++ insecure.js
@@ -8,18 +8,20 @@ const connection = mysql.createConnection({
     database: 'your_database'
 });
 
-// The input to be sanitized
+// The input username to be used in the query
 const username = 'someUsername';
 
 // Secure query using a parameterized query
 const query = 'SELECT * FROM users WHERE username = ?';
 
+// Execute the query with the parameter
 connection.execute(query, [username], (err, results) => {
     if (err) {
         console.error('Error executing query:', err);
         return;
     }
 
+    // Handle the results
     console.log('User data:', results);
 });
 
