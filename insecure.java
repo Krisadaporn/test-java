6af707e493870c173772515455ecd19630870ff4
Krisadaporn Petchdee <krisadaporn.p@gmail.com>
2024-09-03 16:40:24 +0700

    convert to node

--- insecure.java
+++ insecure.java
@@ -1,3 +1,27 @@
-String query = "SELECT * FROM users WHERE username = '" + username + "'";
-Statement stmt = connection.createStatement();
-ResultSet rs = stmt.executeQuery(query);
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'your_password',
+    database: 'your_database'
+});
+
+// The input to be sanitized
+const username = 'someUsername';
+
+// Secure query using a parameterized query
+const query = 'SELECT * FROM users WHERE username = ?';
+
+connection.execute(query, [username], (err, results) => {
+    if (err) {
+        console.error('Error executing query:', err);
+        return;
+    }
+
+    console.log('User data:', results);
+});
+
+// Close the connection
+connection.end();
